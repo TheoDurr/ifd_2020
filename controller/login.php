2d153c32b417d89f0590dd4b5ba99c77ccc67e69
Théo DURR <theo.durr@gmail.com>
2020-10-25 20:31:45 +0100

    Edited errors feature (now uses the session variable)

--- controller/login.php
+++ controller/login.php
@@ -4,13 +4,13 @@ if(!empty($_POST)){
     $uManager = new UserManager($db);
     $result = $uManager->search(array('email' => $_POST['email']));
     if(!$result){
-        $errors['email'] = "Compte introuvable";
+        $_SESSION['errors']['email'] = "Compte introuvable";
     }else{
         if(password_verify($_POST['password'], $result[0]->password())){
             $_SESSION['user'] = $result[0];
             header("Location: index.php");
         }else{
-            $errors['password'] = "Le mot de passe est incorrect";
+            $_SESSION['errors']['password'] = "Le mot de passe est incorrect";
         }
     }
 }
