d4e567a80bda9a11d010678d343ee26e5b343528
Cyrille STROESSER <cyrille.stroesser@orange.fr>
2020-10-27 10:59:43 +0100

    displaying and modifying in DB user's informations functional

--- view/account.php
+++ view/account.php
@@ -6,42 +6,38 @@
 
     <section id="my_info" class="box1">
     <?php if(isset($_GET['modify'])){ ?>
-        <form action="post" id="modify_info">
+        <form method="post" id="modify_info">
             <section>
                 <p class="small_title">Prénom :</p>
-                <input type="text" max="255">
+                <input type="text" max="255" name="firstName" value="<?php echo $_SESSION['user']->firstName(); ?>">
             </section>
             <Section>
                 <p class="small_title">Nom :</p>
-                <input type="text" max="255">
+                <input type="text" max="255" name="lastName" value="<?php echo $_SESSION['user']->lastName(); ?>">
             </Section>
             <section>
                 <p class="small_title">Date de naissance :</p>
-                <input type="date">
+                <input type="date" name="birthDate" value="<?php echo $_SESSION['user']->birthDate(); ?>">
             </section>
             <section>
                 <p class="small_title">Email :</p>
-                <input type="email">
+                <input type="email" name="email" value="<?php echo $_SESSION['user']->email(); ?>">
             </section>
             <input type="submit" value="Valider">
         </form>
-    <?php }else{ ?> 
-        <p id="name_user">Cyrille STROESSER</p>
+    <?php }else{ ?>
+        <p id="name_user"><?php echo $_SESSION['user']->firstName() . " " . $_SESSION['user']->lastName(); ?></p>
             <section>
                 <p class="small_title">Date de naissance :</p>
-                <p>18/06/01</p>
+                <p><?php echo $_SESSION['user']->birthDate(); ?></p>
             </section>
             <section>
                 <p class="small_title">Email :</p>
-                <p>cyrille.stroesser@orange.fr</p>
-            </section>
-            <section>
-                <p class="small_title">Date de naissance</p>
-                <p>18/06/01</p>
+                <p><?php echo $_SESSION['user']->email(); ?></p>
             </section>
             <section>
                 <p class="small_title">Date de création du compte</p>
-                <p>18/10/2020</p>
+                <p> <?php echo $_SESSION['user']->creationDate();?></p>
             </section>
             <a href="index.php?action=account&modify=true">Modifier</a>
        
