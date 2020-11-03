dc407be118ce14d79b33d2f6d57e6d2f1e0589d9
EstebanLELIBOUX <leliboux.esteban@gmail.com>
2020-11-03 16:29:33 +0100

    Traitement du formulaire de recherche d'un jeu

--- controller/games.php
+++ controller/games.php
@@ -1,6 +1,67 @@
 <?php
 
 $gManager = new GameManager($db);
-$data['games'] = $gManager->get();
+$cManager = new CategoryManager($db);
 
+
+if (!empty($_POST)){ // If we received data from the form 
+    if($_POST['search']==''){ // if the user has not typed anything in the search bar
+
+        // the user is not forced to fill in all the boxes, 
+        // if he has not filled in a box, we assign it the maximum or minimum value
+        // so that he does not lose any game in the search. 
+        if( $_POST['priceMin']!='') { $Prmin=$_POST['priceMin']; } else{ $Prmin=0;} 
+        if( $_POST['priceMax']!='') { $Prmax=$_POST['priceMax']; } else{ $Prmax=400;}
+        if( $_POST['playersMin']!='') { $Plmin=$_POST['playersMin']; } else{ $Plmin=0;}
+        if( $_POST['playersMax']!='') { $Plmax=$_POST['playersMax']; } else{ $Plmax=20;}
+
+        $dBt=array(  //the table to be sent to the function is filled in. 
+            'priceMin' => $Prmin,
+            'priceMax' =>$Prmax,
+            'playersMin' => $Plmin,
+            'playersMax' => $Plmax
+        );
+
+        //we search for the game(s) in the database 
+        //whose price and player values are within the requested intervals
+        $dataBetween = $gManager->getBetween($dBt); 
+
+        if($_POST['category']!='0') { // if the user has filled in the category box
+            //we are looking for all the games corresponding to this type of category 
+            $dataCategory=$cManager->get(new Category(array('name' =>$_POST['category'])));
+        }
+        else {
+            //We get all the games
+            $dataCategory=$cManager->get();
+        }
+        $i=0;
+        $dName['games']=array(); // array memorizing names of game // use to comparaison
+        $data['games']=array(); // array memorizing games to send 
+        if($dataBetween!=false && $dataCategory!=false) // if the user has entered both types of data 
+        {
+            foreach($dataBetween as $dtBetween){ // We go through the indexes of the two tables
+                foreach($dataCategory as $dtCategory){
+                    if($dtBetween->category()->id()==$dtCategory->id()){ // If they have the same ID.
+                        if(!in_array($dtBetween->name(),$dName['games'])){ // If the game has not already been selected 
+                            array_push($dName['games'],$dtBetween->name()); // We memorize the name du jeu 
+                            array_push($data['games'],$dtBetween); // We memorize the game and all its characteristics.
+                        }
+                    }
+                }
+            }
+        }
+    
+    }
+        
+    else { // If the user has typed something in the search bar 
+
+        //The game is only searched on what is entered in the search bar.
+        $dataSearch=$gManager->get(new Game(array('name' =>$_POST['search'])));
+        $data['games']=$dataSearch;
+    }  
+}
+else // If the user has sent nothing 
+{
+    $data['games']=$gManager->get();  // We search all games in the database
+}
 require dirname(__FILE__) . '../../view/games.php';
--- view/games.php
+++ view/games.php
@@ -4,24 +4,35 @@
 
 <section class="contents_page">
 
-    <form action="post" class="box1">
+    <form method="post" class="box1" enctype="multipart/form-data">
         <section id="search_area">
             <p class="small_title">Rechercher : </p>
-            <input type="text" name="search" placeholder="Nom, Editeur,...">
+            <input name = "search" list="se" type="text">
+            <datalist id="se">
+                <?php 
+                $gManager = new GameManager($db);
+                $game=$gManager->get(); //We are going to look for all the referenced games in the database.
+                foreach($game as $g){ // They are listed in the search bar ?> 
+                <option value="<?php echo $g->name(); ?>"><?php echo $g->name(); ?></option>
+                <?php } ?>
+            </datalist>
             <p class="small_title">Catégorie :</p>
             <select name="category" id="categroy">
-                <option value="0">-Select-</option>
-                <option value="adventure">Aventure</option>
-                <option value="reflexion">Réflexion</option>
+                <?php 
+                $CManager = new CategoryManager($db);
+                $category=$CManager->get(); //We are going to look for all the referenced category in the database.
+                foreach($category as $c){// They are listed and the user can choose?>
+                <option value="<?php echo $c->name(); ?>"><?php echo $c->name(); ?></option>
+                <?php } ?>
             </select>
             <p class="small_title">Prix min:</p>
-            <input type="number" name="min_price" min="1" max="400" step="1">
+            <input type="number" name="priceMin" min="1" max="400" step="1">
             <p class="small_title">Prix max:</p>
-            <input type="number" name="max_price" min="1" max="400" step="1">
+            <input type="number" name="priceMax" min="1" max="400" step="1">
             <p class="small_title">Nb joueur min :</p>
-            <input type="number" name="players_min" min="1" max="20" step="1">
+            <input type="number" name="playersMin" min="1" max="20" step="1">
             <p class="small_title">Nb joueur max :</p>
-            <input type="number" name="players_max" min="1" max="20" step="1">
+            <input type="number" name="playersMax" min="1" max="20" step="1">
             <input type="submit" value="Rechercher">
         </section>
         <p id="indication">*Vous n'êtes pas obligé de renseigner tous les champs    </p>
@@ -35,8 +46,8 @@
                 <p id="results_title">Jeux</p>
             <?php }
         ?>
-
-        <?php foreach($data['games'] as $game){ ?>
+        <?php if(!empty($data['games'])){ // If we found one or more games?>
+        <?php foreach($data['games'] as $game){ // We show the different games?>
         <a href="index.php?action=game_page&id=<?= $game->id() ?>" class="result_game">
             <img src="data:image/jpeg;base64, <?= base64_encode($game->img())?>"/>
             <section>
@@ -58,7 +69,9 @@
             </section>
         </a>
         <?php } ?>
-
+        <?php } else {// If no game was found we mark no result?>
+            <p class="big_title">Aucun Résultat</p>
+            <?php }?>
     </section>
 
 </section>
